d206fdfcde3b07211594b893905d8e56edc5089c
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-12-30 09:54:52 -0500

    🍱 Update assets

---
+++ src/pages/_document.tsx
@@ -0,0 +1,31 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://use.typekit.net/lih5yhc.css"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
