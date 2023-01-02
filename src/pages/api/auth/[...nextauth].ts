13b68c80bd5e220a43f26fc84d7a93e6bd737bae
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-01-02 00:13:52 -0500

    🔒️ Allow automatic account linking

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -23,10 +23,12 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     GitHubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     // ...add more providers here
   ],
