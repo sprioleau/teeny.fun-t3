118d5ed33a588dd9554e18a991a205a4f4a2e775
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-01-01 23:12:25 -0500

    ♻️ Simplify page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { BsArrowUpRight } from "react-icons/bs";
 import { useSession } from "next-auth/react";
 import {
-  AuthActionButton,
-  Footer,
-  Logo,
   UrlForm,
+  UrlTable,
   // Shapes
 } from "@components";
 
-import { formatAsDomainName, trpc } from "@utils";
+import { trpc } from "@utils";
 
 const HomePage: NextPage = () => {
   const { data: users } = trpc.user.getAllUsers.useQuery();
@@ -30,76 +27,11 @@ const HomePage: NextPage = () => {
         <title>Teeny.fun</title>
       </Head>
 
-      <main className="main">
-        <AuthActionButton />
-        <div className="home">
-          <h1>
-            <span
-              aria-hidden={true}
-              hidden={true}
-            >
-              teeny.fun
-            </span>
-            <span
-              style={{
-                width: "20rem",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Logo size="15rem" />
-            </span>
-          </h1>
-          {Boolean(teenyUrls?.length) && (
-            <div>
-              <h3>Top 5 URLs</h3>
-              <table>
-                <thead>
-                  <tr>
-                    <th>teeny.fun/</th>
-                    <th>Long URL</th>
-                    <th>Hits</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {[...teenyUrls]
-                    .map(({ id, teenyCode, shortUrl, longUrl, hits }) => (
-                      <tr key={id}>
-                        <td>
-                          <a
-                            href={teenyCode}
-                            title={shortUrl}
-                          >
-                            {teenyCode}
-                            <BsArrowUpRight />
-                          </a>
-                        </td>
-                        <td>
-                          <a
-                            href={longUrl}
-                            title={longUrl}
-                          >
-                            {formatAsDomainName(longUrl)}
-                          </a>
-                        </td>
-                        <td>{hits}</td>
-                      </tr>
-                    ))
-                    .slice(0, 5)}
-                </tbody>
-              </table>
-            </div>
-          )}
-          {/* <Shapes /> */}
-          <UrlForm />
-          {/* <p>
-            <Link href="/">Sign up</Link> to update your links.
-          </p>
-          <p>
-            <Link href="/">Already have an account?</Link>
-          </p> */}
-          {/* {topEmojis && (
+      <main className="home page">
+        <UrlForm />
+        {Boolean(teenyUrls?.length) && <UrlTable urls={teenyUrls} />}
+        {/* <Shapes /> */}
+        {/* {topEmojis && (
               <table>
                 <thead>
                   <tr>
@@ -120,10 +52,7 @@ const HomePage: NextPage = () => {
                 </tbody>
               </table>
             )} */}
-        </div>
       </main>
-
-      <Footer />
     </>
   );
 };
