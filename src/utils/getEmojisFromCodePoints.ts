2e057d5fd02bd7059e01aa4801b11e807a017a50
San'Quan Prioleau <sq.prioleau@gmail.com>
2023-01-02 22:49:54 -0500

    ⚰️ Remove dead code

--- src/utils/getEmojisFromCodePoints.ts
+++
@@ -1,5 +0,0 @@
-const getEmojisFromCodePoints = (codePointsString: string): string => {
-	return String.fromCodePoint(...codePointsString.split(" ").map((n) => Number(n)));
-};
-
-export default getEmojisFromCodePoints;
